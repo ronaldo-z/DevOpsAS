5e4846b806d5543ee664c30ab6f2e7cfaf876bcd
ronaldo-z <[ronaldo.ziger@gmail.com]>
2026-09-07 16:35:22 -0300

    Correção do value do athletico (/templates/index.hmtl) e adição da rota dinêmica para exibir o time escolhido.

--- main.py
+++ main.py
@@ -11,5 +11,12 @@ def escolher():
     time = request.form.get('time')
     return redirect(url_for('pagina_time', nome_time=time))
 
+@app.route('/time/<nome_time>')
+def pagina_time(nome_time):
+    times_capital = ['coritiba', 'athletico', 'parana']
+    if nome_time in times_capital:
+        return f"<h1>Bem-vindo, torcedor do {nome_time.capitalize()}!</h1>"
+    return "<h1>Time não encontrado na capital!</h1>", 404
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
